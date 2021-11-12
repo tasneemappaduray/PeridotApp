9ee8deb7c226b04725bdc568cf37c3a1827d0a37
tasneemappaduray <40742768+tasneemappaduray@users.noreply.github.com>
2021-11-12 17:44:14 +0200

    Dirty functional commit

---
+++ app/src/main/java/com/example/peridotapp/PushNotificationService.java
@@ -0,0 +1,35 @@
+package com.example.peridotapp;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class PushNotificationService extends FirebaseMessagingService {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        NotificationChannel notificationChannel = new NotificationChannel(
+                "HEADS_UP_NOTIFICATIONS",
+                "MyNotification",
+                NotificationManager.IMPORTANCE_HIGH);
+        getSystemService(NotificationManager.class).createNotificationChannel(notificationChannel);
+        Notification.Builder notification = new Notification.Builder(this, "HEADS_UP_NOTIFICATIONS")
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.notification_foreground)
+                .setAutoCancel(true);
+        NotificationManagerCompat.from(this).notify(1, notification.build());
+    }
+}
